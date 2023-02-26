1c9d387498632e6e507fcc21fdd65351f84791fc
lasha petriashvili <lasha.petro1@gmail.com>
2023-02-26 20:09:46 +0400

    WIP

--- src/actions/auto-merge.ts
+++ src/actions/auto-merge.ts
@@ -3,33 +3,10 @@ import * as core from '@actions/core';
 import * as github from '@actions/github';
 import { Inputs, Strategy, ReviewerBySate, Reviewer } from '../config/typings';
 import { info, debug, warning, error } from '../logger';
-import {
-  getReviewsByGraphQL,
-  removeDuplicateReviewer,
-  filterReviewersByState,
-  getLatestCommitDate,
-} from '../github';
-
-async function fetchGithubReviews(token: string) {
-  const octokit = github.getOctokit(token);
-  const { owner, repo } = github.context.repo;
-  // TODO Fix Typescript Error
-  // @ts-ignore
-  const pull_number = github.context.payload.pull_request.number;
-  const reviews = await octokit.pulls.listReviews({
-    owner,
-    repo,
-    pull_number,
-  });
-  info('---------- fetchGithubReviews ---------------');
-  info(JSON.stringify(reviews, null, 2));
-}
 
 export async function run(): Promise<void> {
   try {
     info('Staring PR auto merging.');
-
-    let doNotMerge = false;
 
     const [owner, repo] = core.getInput('repository').split('/');
 
@@ -43,106 +20,55 @@ export async function run(): Promise<void> {
       token: core.getInput('token', { required: true }),
     };
 
-    debug(`Inputs: ${inspect(configInput)}`);
+    // Get only then reviewers then not approved
+    const reviewers = {
+      approved: [],
+      changes_requested: [],
+      commented: [],
+      dismissed: [],
+      pending: [],
+    };
 
-    const client = github.getOctokit(configInput.token);
+    const octokit = github.getOctokit(configInput.token);
 
-    const { data: pullRequest } = await client.pulls.get({
+    // @ts-ignore
+    const { data: pullRequest } = await octokit.pulls.get({
       owner,
       repo,
       pull_number: configInput.pullRequestNumber,
     });
-    info('Checking requested reviewers.');
 
-    // TODO Fix Typescript
-    // @ts-ignore
-    const res = await getLatestCommitDate(pullRequest);
-
-    info(JSON.stringify(res, null, 2));
-
-    return;
-
-    if (pullRequest?.requested_reviewers) {
-      /* const requestedChanges = pullRequest?.requested_reviewers?.map( */
-      /*   (reviewer) => reviewer.login, */
-      /* ); */
-      /**/
-      /* info('---------- pullRequest.requested_reviewers ---------------'); */
-      /* info(JSON.stringify(pullRequest?.requested_reviewers, null, 2)); */
-      /* if (requestedChanges.length > 0) { */
-      /*   warning(`Waiting [${requestedChanges.join(', ')}] to approve.`); */
-      /*   doNotMerge = true; */
-      /*   return; */
-      /* } */
-    }
-
-    info('Checking required changes status.');
-
-    // TODO Fix Typescript Error
-    // @ts-ignore
-    const reviewers: Reviewer[] = await getReviewsByGraphQL(pullRequest);
-
-    const reviewersByState: ReviewerBySate = filterReviewersByState(
-      removeDuplicateReviewer(reviewers),
-      reviewers,
-    );
-
-    info('----------------- reviewersByState ---------------');
-    info(JSON.stringify(reviewersByState, null, 2));
-
-    if (reviewersByState.requiredChanges.length) {
-      warning(`${reviewersByState.requiredChanges.join(', ')} required changes.`);
-      doNotMerge = true;
-    }
-
-    info('Checking CI status.');
-
-    const { data: checks } = await client.checks.listForRef({
-      owner: configInput.owner,
-      repo: configInput.repo,
-      ref: configInput.sha,
-    });
-
-    const totalStatus = checks.total_count;
-    const totalSuccessStatuses = checks.check_runs.filter(
-      (check) => check.conclusion === 'success' || check.conclusion === 'skipped',
-    ).length;
-
-    if (totalStatus - 1 !== totalSuccessStatuses) {
-      warning(
-        `Not all status success, ${totalSuccessStatuses} out of ${
-          totalStatus - 1
-        } (ignored this check) success`,
-      );
-      doNotMerge = true;
-    }
-
-    if (doNotMerge) {
-      return;
-    }
-
-    if (configInput.comment) {
-      const { data: resp } = await client.issues.createComment({
-        owner: configInput.owner,
-        repo: configInput.repo,
-        issue_number: configInput.pullRequestNumber,
-        body: configInput.comment,
-      });
-
-      debug(`Post comment ${inspect(configInput.comment)}`);
-      core.setOutput('commentID', resp.id);
-    }
-
-    info('Merging...');
-
-    await client.pulls.merge({
+    const { data: reviews } = await octokit.pulls.listReviews({
       owner,
       repo,
       pull_number: configInput.pullRequestNumber,
-      merge_method: configInput.strategy,
     });
 
-    core.setOutput('merged', true);
+    reviews.forEach((review) => {
+      // @ts-ignore
+      reviewers[review.state].push({ login: review.user.login });
+    });
+
+    const { data: comments } = await octokit.issues.listComments({
+      owner,
+      repo,
+      issue_number: configInput.pullRequestNumber,
+    });
+
+    comments.forEach((comment) => {
+      // @ts-ignore
+      reviewers.commented.push({ login: comment.user.login });
+    });
+
+    // @ts-ignore
+    const { data: commits } = await octokit.pulls.listCommits({
+      owner,
+      repo,
+      pull_number: configInput.pullRequestNumber,
+    });
+
+    info(JSON.stringify(reviewers, null, 2));
+    info(JSON.stringify(commits, null, 2));
   } catch (err) {
     error(err as Error);
   }
